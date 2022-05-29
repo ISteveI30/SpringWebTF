62ba5a06496ab5bc1203200cba452bfe287d1a0f
brajan <brajansteve@gmail.com>
2022-05-28 20:45:17 -0500

    Codificación del backend de la aplicacion

--- src/main/java/pe/edu/upc/uhelp/controller/PagoController.java
+++ src/main/java/pe/edu/upc/uhelp/controller/PagoController.java
@@ -1,5 +1,80 @@
 package pe.edu.upc.uhelp.controller;
 
-public class PagoController {
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.uhelp.entities.Pago;
+import pe.edu.upc.uhelp.serviceinterface.IPagoService;
+
+@Controller
+@RequestMapping("/pagos")
+public class PagoController {
+	
+	@Autowired
+	private IPagoService pagoService;
+	List<Pago> lstPagos;
+	
+	@GetMapping("/registro")
+	public String newPago(Model model) {
+		model.addAttribute("pago",new Pago());
+		return "pago/registroPago";
+	}
+	@PostMapping("/guardar")
+	public String savePago(@Valid Pago pago,BindingResult binRes,Model model) {
+		if(binRes.hasErrors()) {
+			return "pago/registroPago";	
+		}else {
+			pagoService.insert(pago);
+			model.addAttribute("mensaje","Se registró correctamente!!");
+			return "redirect:/pagos/registro";
+		}		
+	}
+	@GetMapping("/listar")
+	public String listPago(Model model) {
+		try {
+			model.addAttribute("lstpagos",pagoService.list());
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+		}
+		return "pago/listPago";
+	}
+	@RequestMapping("/editar/{id}")
+	public String UpdatePago(@PathVariable("id")int id, Model model) {
+		try {
+			Optional<Pago> pago=pagoService.findPago(id);
+			model.addAttribute("per",pago.get());
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+		}
+		return "pago/updatePago";
+	}
+	@RequestMapping("/eliminar")
+	public String deletePago(@PathVariable("id")int id, Model model) {
+		try {
+			if(id>0) {
+				pagoService.delete(id);
+				model.addAttribute("lstpagos",pagoService.list());
+			}
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+		}
+		return "redirect:/pagos/listar";
+	}
+	@PostMapping("/modificar")
+	public String updatePago(Pago pago) {
+			pagoService.update(pago);
+			return "redirect:/pagos/listar";
+	}
 }
--- src/main/java/pe/edu/upc/uhelp/serviceimplements/PagoServiceImpl.java
+++ src/main/java/pe/edu/upc/uhelp/serviceimplements/PagoServiceImpl.java
@@ -1,5 +1,46 @@
 package pe.edu.upc.uhelp.serviceimplements;
 
-public class PagoServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.edu.upc.uhelp.entities.Pago;
+import pe.edu.upc.uhelp.repositories.IPagoRepository;
+import pe.edu.upc.uhelp.serviceinterface.IPagoService;
+
+@Service
+public class PagoServiceImpl implements IPagoService {
+
+	/*Como los metodo crud ya provienen del japaRepository no es ncesario 
+	 *declararlos en el Repository*/
+	@Autowired
+	private IPagoRepository pagoRepository;
+	
+	@Override
+	public void insert(Pago person) {
+		pagoRepository.save(person);
+	}
+
+	@Override
+	public List<Pago> list() {
+		return pagoRepository.findAll();
+	}
+
+	@Override
+	public void delete(int idpago) {
+		pagoRepository.deleteById(idpago);
+	}
+
+	@Override
+	public Optional<Pago> findPago(int idpago) {
+		return pagoRepository.findById(idpago);
+	}
+
+	@Override
+	public void update(Pago pago) {
+		pagoRepository.save(pago);
+	}
 
 }
