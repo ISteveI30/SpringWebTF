455504ce4d091d174f82819ab93255e01c0b5816
Luis <luisernestoguardalessolano@gmail.com>
2022-05-29 22:05:47 -0500

    Creacion y desarrollo del ServiceImplement para entidad Sede

--- src/main/java/pe/edu/upc/uhelp/serviceinterface/ISedeService.java
+++ src/main/java/pe/edu/upc/uhelp/serviceinterface/ISedeService.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.uhelp.serviceinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.edu.upc.uhelp.entities.Sede;
 
@@ -11,4 +12,8 @@ public interface ISedeService {
 	public List <Sede> list();
 	
 	public void delete (int idPerson);
+	
+	Optional<Sede> listId(int idSede);
+	
+	public void update(Sede sede);
 }
